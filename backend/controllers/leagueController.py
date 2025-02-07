52fa99e6a49ebe9f9fd931d0a29b12bc22463e96
maelbadet <maelbadet21@gmail.com>
2025-02-07 14:20:12 +0100

    add : image to get All leagues

--- backend/controllers/leagueController.py
+++ backend/controllers/leagueController.py
@@ -9,7 +9,14 @@ league_router = APIRouter()
 @league_router.get("/getAll", response_model=list)
 def get_all_leagues(db: Session = Depends(get_db)):
 	leagues = db.query(League).all()
-	return [{"id": l.id, "name": l.name, "country_id": l.country_id} for l in leagues]
+	return [
+		{"id": l.id,
+		 "name": l.name,
+		 "country_id": l.country_id,
+		 "image_path": l.image_path
+		 }
+		for l in leagues
+	]
 
 
 @league_router.get("/get/{league_id}", response_model=dict)
@@ -17,4 +24,9 @@ def get_league_by_id(league_id: int, db: Session = Depends(get_db)):
 	league = db.query(League).filter_by(id=league_id).first()
 	if not league:
 		raise HTTPException(status_code=404, detail="League not found")
-	return {"id": league.id, "name": league.name, "country_id": league.country_id}
+	return {
+		"id": league.id,
+		"name": league.name,
+		"country_id": league.country_id,
+		"image_path": league.image_path
+	}
